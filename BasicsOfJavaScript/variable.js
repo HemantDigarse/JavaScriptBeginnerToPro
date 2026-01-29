1445ee08b39181ad51783cac63ca4f2d7f89bbc8
HemantDigarse <150275819+HemantDigarse@users.noreply.github.com>
2026-01-29 18:25:16 +0000

    Variables

---
+++ BasicsOfJavaScript/variable.js
@@ -0,0 +1,13 @@
+const accountID=45215;
+let accountEmail="hemant@gmail.com";
+var accountPass=123456;
+
+accountCity="Chhindwara";
+let accountState;
+
+console.log(accountID);
+
+console.table([accountID,accountEmail,accountPass,accountCity,accountState]);
+
+
+// Prefer not to use var because of issue of block scope and functional scope
