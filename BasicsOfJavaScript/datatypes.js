2b8068f64fba17884f7710935f17181325d61fc6
HemantDigarse <150275819+HemantDigarse@users.noreply.github.com>
2026-01-29 18:36:12 +0000

    DataTypes

---
+++ BasicsOfJavaScript/datatypes.js
@@ -0,0 +1,32 @@
+// Datatypes
+"use strict"; // treat all js code as newer version
+
+// alert("hello");  We are using node.js not browser
+
+let name="Hemant"
+let age=21
+let isLoggedTrue=true
+
+/*Type of DataTypes
+Primitives Data Types
+Number-> 2 to the power 53
+BigInt
+String=> ""
+Boolean=>true/false
+null ->standalone value
+undefined -> variable declared but value not defined
+Symbol=> unique
+
+Objects
+
+
+*/
+
+console.log(typeof null)
+// Type of null is Object
+console.log(typeof undefined)
+// Type of null is Undefined
+
+
+
+
